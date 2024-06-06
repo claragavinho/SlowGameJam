2ac1ebf9101ad3bd6b89da1c043e20999969e7fd
safdfadf <maahuja123@gmail.com>
2024-06-06 16:39:43 -0600

    Background Fix

--- SlowGameJam/Assets/Scripts/BackGround/BackGround.cs
+++ SlowGameJam/Assets/Scripts/BackGround/BackGround.cs
@@ -21,11 +21,11 @@ public class BackGround : MonoBehaviour
         {
             Debug.Log("Camera Control not assigned");
         }
-        Startpos = new Vector3(0, 6.45f, -0.5f);
+        Startpos = new Vector3(0, 6.45f, 0);
      SpawnedBackground=Instantiate(BackGroundPrefab[0], Startpos, BackGroundPrefab[0].transform.rotation);
     }
     private void Update()
     {
-           SpawnedBackground.transform.position = new Vector3(transform.position.x, cameraControl.transform.position.y,transform.position.z );
+           SpawnedBackground.transform.position = new Vector3(transform.position.x, cameraControl.transform.position.y,transform.position.z);
     }
 }
